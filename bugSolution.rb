948987b60e551355ff9c06aaa4a6891041522f76
BugHunter-X <askzenai@gmail.com>
2025-01-25 18:54:07 +0100

    Add bugSolution.rb file

---
+++ bugSolution.rb
@@ -0,0 +1,24 @@
+```ruby
+class MyClass
+  def initialize(value)
+    @value = value
+  end
+
+  def value
+    @value
+  end
+
+  def value=(new_value)
+    @value = new_value
+  end
+end
+
+my_object = MyClass.new(10)
+puts my_object.value # => 10
+
+my_object.value = 20
+puts my_object.value # => 20
+
+my_object.value = 30
+puts my_object.value # => 30
+```
